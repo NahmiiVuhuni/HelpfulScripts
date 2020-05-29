7b6cfe86a813afc8dbf466c2cc8f241e4e8a3e97
Hubok <me@hubok.net>
2020-05-29 08:52:54 -0400

    Add vyos-build-1.2.0.sh
    Adds a script for building VyOS LTS 1.2.x that runs on Linux and WSL 2 from within Linux.

---
+++ Docker/vyos-build-1.2.0.sh
@@ -0,0 +1,24 @@
+# Runs on normal Linux distros with Bash, and WSL 2 distros from within their terminal. Do not run on Docker for Windows.
+#!/bin/bash
+set +x
+
+if [ -d "vyos-build" ]
+then
+    sudo rm -rf vyos-build
+fi
+
+git clone -b crux --single-branch https://github.com/vyos/vyos-build
+cd vyos-build
+docker run -it --rm -d --name vyos-build-1.2.0 --privileged -v $(pwd):/vyos -w /vyos vyos/vyos-build:crux bash
+
+docker exec -it vyos-build-1.2.0 ./configure --architecture amd64 --build-by "insert@email.here" --build-type release --version 1.2.0
+docker exec -it vyos-build-1.2.0 sudo make iso
+docker stop vyos-build-1.2.0
+
+cp build/vyos-1.2.0-amd64.iso ..
+cd ..
+
+if [ -d "vyos-build" ]
+then
+    sudo rm -rf vyos-build
+fi
