72bb139d2c69a1282ec6c3e3aeead4f9417bc750
Nahmii Vuh'uni <git@nahmii.dog>
2024-07-18 10:37:59 -0700

    Fixed off by one error in random_selcal.go

--- Flightsim/random_selcal.go
+++ Flightsim/random_selcal.go
@@ -12,25 +12,25 @@ func main() {
 	l := [16]string{"A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "P", "Q", "R", "S"}
 
 	//Generate 1st letter and fill all spots with the same one
-	r := rand.IntN(15)
+	r := rand.IntN(16)
 	c := [4]int{r, r, r, r}
 
 	//Generate 2nd letter until it's different from the 1st
 	for c[1] == c[0] {
-		c[1] = rand.IntN(15)
+		c[1] = rand.IntN(16)
 	}
 
 	//Generate 3rd letter until it's different from 1 and 2
 	for c[2] == c[0] ||
 		c[2] == c[1] {
-		c[2] = rand.IntN(15)
+		c[2] = rand.IntN(16)
 	}
 
 	//Generate 3rd letter until it's different from 1, 2, and 3
 	for c[3] == c[0] ||
 		c[3] == c[1] ||
 		c[3] == c[2] {
-		c[3] = rand.IntN(15)
+		c[3] = rand.IntN(16)
 	}
 
 	//Reverse 1st and 2nd letter if not in order
